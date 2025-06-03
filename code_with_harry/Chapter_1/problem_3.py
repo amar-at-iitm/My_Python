0f18ca7d40091ffb432f2845b3269d33c8cf0e99
Amar <ma24m002@smail.iitm.ac.in>
2025-06-04 01:00:11 +0530

    problem 3rd

---
+++ code_with_harry/Chapter_1/problem_3.py
@@ -0,0 +1,6 @@
+#  Install an external module and use it to perform an operation of your interest. 
+
+import pyjokes
+
+# This code prints a random joke using the pyjokes library
+print(pyjokes.get_joke())
