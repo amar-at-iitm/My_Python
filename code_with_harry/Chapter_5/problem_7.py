2317bf035b8e910e55a0c860b6a71ca1461d5a37
Amar <ma24m002@smail.iitm.ac.in>
2025-06-08 12:45:48 +0530

    problem 7

---
+++ code_with_harry/Chapter_5/problem_7.py
@@ -0,0 +1,9 @@
+"""
+Q. If the names of 2 friends are same; what will happen to the program in problem 6?
+Ans: If two name will be same then the first name and its favourite language will be 
+replaced by 2nd name and its favourite language.
+
+
+*** To verify this Enter any name twice with different favourite language in the programe 6.
+
+ """
