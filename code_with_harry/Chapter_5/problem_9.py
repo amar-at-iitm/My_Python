9268ccd4a00f6566fd55f95e74d70dcbf933e276
Amar <ma24m002@smail.iitm.ac.in>
2025-06-08 12:56:50 +0530

    problem 9

---
+++ code_with_harry/Chapter_5/problem_9.py
@@ -0,0 +1,10 @@
+"""
+Q. Can you change the values inside a list which is contained in set S?  
+s = {8, 7, 12, "Harry", [1,2]}
+
+Ans: No, we cannot directly change the values inside a list which is contained 
+within a set S in Python, because sets can only contain immutable (hashable) elements.
+
+"""
+
+
