788b259ec5fcb3be1076c2d90c55453996aa3d83
Amar <ma24m002@smail.iitm.ac.in>
2025-06-08 12:52:47 +0530

    problem 7 and 8

---
+++ code_with_harry/Chapter_5/problem_7_8.py
@@ -0,0 +1,17 @@
+"""
+Q.7 If the names of 2 friends are same; what will happen to the program in problem 6?
+Ans: If two name will be same then the first name and its favourite language will be 
+replaced by 2nd name and its favourite language.
+
+
+
+Q8. If languages of two friends are same; what will happen to the program in problem 6?
+Ans: In this case both name will be stored but both will have the same favourite language. 
+
+
+This happens because dictionary follow one to many relations, not many to one. 
+
+
+*** To verify this run the programe 6, enter the names and favourite languages accordingly.
+
+"""
