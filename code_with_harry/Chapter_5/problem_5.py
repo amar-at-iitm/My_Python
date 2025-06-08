45a58a96f9c3903f71d29d4a6f8fde3f9d306758
Amar <ma24m002@smail.iitm.ac.in>
2025-06-08 12:10:03 +0530

    problem 5

---
+++ code_with_harry/Chapter_5/problem_5.py
@@ -0,0 +1,9 @@
+"""
+Q. s = {}, What is the type of 's'?
+Ans: This is how, we initialize a dictionary. Sicnce there is no element, so this is an empty dictionary.
+Thus the type of s is dictionary.
+
+To verify this, run this program. 
+"""
+s = {}
+print(type(s))
