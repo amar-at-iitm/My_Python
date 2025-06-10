8b90d6995e899ebfab15336106bc352c0a27bd6b
Amar <ma24m002@smail.iitm.ac.in>
2025-06-10 12:30:57 +0530

    problem 6.7

---
+++ code_with_harry/Chapter_6/problem_7.py
@@ -0,0 +1,6 @@
+# Write a program to find out whether a given post is talking about “Harry” or not.
+
+# url = input("Enter the link of the post")
+post_text = input("Enter the text of the post")
+if "Harry" in post_text:
+    print("Yes, this post talks about Harry")
