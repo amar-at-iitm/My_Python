010fdb6671e66dc55b5eb591ea4743dd041f36e5
Amar <ma24m002@smail.iitm.ac.in>
2025-06-13 12:26:54 +0530

    problem 7.9

---
+++ code_with_harry/Chapter_7/problem_9.py
@@ -0,0 +1,11 @@
+"""
+Q. Write a program to print the following star pattern. 
+* * * 
+*   *    for n = 3 
+* * *  
+"""
+
+n = 3
+
+for i in range(1,n+1):
+    print("*"+(i%2)*"*"+((i+1)%2)*" "+"*")
