1475bef5ab8f50c1ac09570dac93506bf1c1c9c2
Amar <ma24m002@smail.iitm.ac.in>
2025-06-19 22:30:15 +0530

    problem 9.11

--- code_with_harry/Chapter_9/problem_11.py
+++ code_with_harry/Chapter_9/problem_11.py
@@ -8,5 +8,5 @@ print("This file will rename the existing '.txt' file")
 with open("file_to_rename.txt","w") as f:
     f.write("Renaming old file name 'file_to_rename' to 'renamed_by_python.txt")
 
-os.rename("file_to_rename.txt", "remaned_by_python.txt")
+os.rename("file_to_rename.txt", "renamed_by_python.txt")
 print("The file 'file_to_rename.txt' is ranamed to 'rename_by_python.txt' and saved successfully")
