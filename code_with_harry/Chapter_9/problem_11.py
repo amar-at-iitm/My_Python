f659f0c86aed7df49d5a5e74ba11e8f7fc54fb23
Amar <ma24m002@smail.iitm.ac.in>
2025-06-19 22:27:44 +0530

    problem 9.11

---
+++ code_with_harry/Chapter_9/problem_11.py
@@ -0,0 +1,12 @@
+#  Write a python program to rename a file to “renamed_by_ python.txt.
+
+import os
+
+
+print("This file will rename the existing '.txt' file")
+
+with open("file_to_rename.txt","w") as f:
+    f.write("Renaming old file name 'file_to_rename' to 'renamed_by_python.txt")
+
+os.rename("file_to_rename.txt", "remaned_by_python.txt")
+print("The file 'file_to_rename.txt' is ranamed to 'rename_by_python.txt' and saved successfully")
